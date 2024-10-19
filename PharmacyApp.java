ce66ada6215e946104fbf9d224ae0b0c98b58077
eldawi <63516988+eldawi@users.noreply.github.com>
2024-10-19 23:31:28 +0300

    Update

---
+++ PharmacyApp.java
@@ -0,0 +1,10 @@
+package com.mycompany.pharmacyapp;
+
+public class PharmacyApp {
+
+    public static void main(String[] args) {
+        Pharmacy pharmacy = new Pharmacy();
+        pharmacy.setCapacity();
+
+    }
+}
